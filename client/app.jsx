5580e060e57822e8965cf34c11732f5c95465621
Lance <ltnlikespie@gmail.com>
2023-04-18 02:37:33 -0400

    added real time drawing and the ability to join rooms via socket io

--- client/app.jsx
+++ client/app.jsx
@@ -1,5 +1,6 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+import { useEffect } from 'react';
 
 const root = document.querySelector('#content');
 let socket;
@@ -22,7 +23,7 @@ function CredentialsPage() {
                         headers: { 'Content-Type': 'application/json' },
                         body: JSON.stringify({ user, pass })
                     });
-                    res.status === 200 ? ReactDOM.render(<HubPage />, root) : console.log("login failed");    
+                    res.status === 200 ? ReactDOM.render(<HubPage />, root) : console.log("login failed");
                     return false;
                 }}
                 action="/login"
@@ -53,7 +54,7 @@ function CredentialsPage() {
                             headers: { 'Content-Type': 'application/json' },
                             body: JSON.stringify({ user, pass })
                         });
-                        res.status === 201 ? ReactDOM.render(<HubPage />, root) : console.log("signup failed");    
+                        res.status === 201 ? ReactDOM.render(<HubPage />, root) : console.log("signup failed");
                     }
                     return false;
                 }}
@@ -79,8 +80,13 @@ function HubPage() {
             <a href="/logout">Logout</a>
             <h1>Join Room</h1>
             <form onSubmit={(e) => {
-                const roomName = e.target.querySelector("#roomName").value;
+                e.preventDefault();
                 socket = io();
+                const roomName = e.target.querySelector("#roomName").value;
+                //socket.emit('join room', roomName);
+                
+                ReactDOM.render(<GamePage />, root);
+                return false;
             }}>
                 <label htmlFor="roomName">Room Name: </label>
                 <input id="roomName" type="text"></input>
@@ -88,8 +94,10 @@ function HubPage() {
             </form>
             <h1>Create Room</h1>
             <form onSubmit={(e) => {
+                /*socket = io();
                 const roomName = e.target.querySelector("#roomName").value;
-                socket = io();
+                socket.emit('create room', roomName);
+                return false;*/
             }}>
                 <label htmlFor="newRoomName">New Room Name: </label>
                 <input id="newRoomName" type="text"></input>
@@ -99,13 +107,97 @@ function HubPage() {
     );
 }
 
-/*function GamePage() {
+function GamePage() {
+    //https://daveceddia.com/react-hook-after-render/
+    useEffect(() => {
+        /* //https://stackoverflow.com/questions/26212792/convert-an-image-to-canvas-that-is-already-loaded
+         let image = document.createElement('img');
+         document.body.appendChild(image);
+         image.setAttribute('style','display:none');
+         image.setAttribute('alt','script div');
+         image.setAttribute("src", drawing);
+         ctx.drawImage(image,0,0,image.width,image.height);
+         document.body.removeChild(image);
+        socket.emit('drawing', image);*/
+        // Setup canvas
+        const cvs = document.querySelector("canvas");
+        // Please try making the pixel size a proper divisor of the canvas
+        const pixelSize = 20;
+        const ctx = cvs.getContext("2d");
+        ctx.fillStyle = "black";
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 0.125;
+        // Make grid lines so user can anticipate where their pixel is being placed
+        for (let i = 0; i < cvs.height / pixelSize; i++) {
+            ctx.beginPath();
+            ctx.moveTo(0, i * pixelSize);
+            ctx.lineTo(cvs.width, i * pixelSize);
+            ctx.closePath();
+            ctx.stroke();
+        }
+        for (let i = 0; i < cvs.width / pixelSize; i++) {
+            ctx.beginPath();
+            ctx.moveTo(i * pixelSize, 0);
+            ctx.lineTo(i * pixelSize, cvs.height);
+            ctx.closePath();
+            ctx.stroke();
+        }
+        // BORROWED CODE
+        // workaround for events only firing once
+        // simulates them firing multiple times
+        // let's user continuously draw while holding down mouse button
+        // instead of having to click everytime to place a pixel
+        // https://stackoverflow.com/questions/41304737/why-onmousedown-event-occur-once-how-to-handle-mouse-hold-event
+        let pixelX;
+        let pixelY;
+        let timer;
+        cvs.addEventListener("mousemove", (e) => {
+            // put canvas coords in global space
+            const cvsPos = cvs.getBoundingClientRect();
+            const cvsX = cvsPos.x;
+            const cvsY = cvsPos.y;
+            const canXAbs = cvsX + window.scrollX;
+            const canYAbs = cvsY + window.scrollY;
+            // put mouse coords relative to canvas space
+            const cvsMouseX = e.pageX - canXAbs;
+            const cvsMouseY = e.pageY - canYAbs;
+            // Math.trunc ensures that pixel doesn't take up multiple gridboxes 
+            // especially in the event of a pixel size that is not a proper divisor of the canvas dimensions
+            pixelX = pixelSize * Math.trunc(cvsMouseX / pixelSize);
+            pixelY = pixelSize * Math.trunc(cvsMouseY / pixelSize);
+        });
+        cvs.addEventListener("mousedown", () => {
+            timer = setInterval(() => {
+                ctx.fillRect(pixelX, pixelY, pixelSize, pixelSize);
+                socket.emit('drawing', cvs.toDataURL("image/png"));
+            });
+        });
+        function mouseDone() {
+            clearInterval(timer);
+        }
+        cvs.addEventListener("mouseup", mouseDone);
+        cvs.addEventListener("mouseleave", mouseDone);
+        // END OF BORROWED CODE
+        socket.on('drawing', (drawing) => {
+            //https://stackoverflow.com/questions/26212792/convert-an-image-to-canvas-that-is-already-loaded
+            let image = document.createElement('img');
+            document.body.appendChild(image);
+            image.setAttribute('style','display:none');
+            image.setAttribute('alt','script div');
+            image.setAttribute("src", drawing);
+            ctx.drawImage(image,0,0,image.width,image.height);
+            document.body.removeChild(image);
+        })
+    });
     return (
-        <button></button>
+        <>
+            <canvas width="500" height="500"></canvas>
+        </>
     );
-}*/
+}
 
 window.onload = async () => {
-    const res = await fetch('/session', {method: 'HEAD'});
-    res.status === 200 ? ReactDOM.render(<HubPage/>, root) : ReactDOM.render(<CredentialsPage/>, root);
+    const res = await fetch('/session', { method: 'HEAD' });
+    res.status === 200 ? ReactDOM.render(<HubPage />, root) : ReactDOM.render(<CredentialsPage />, root);
+
 };
--- server/io.js
+++ server/io.js
@@ -3,22 +3,28 @@ const { Server } = require('socket.io');
 
 let io;
 
-const handleResult = (result) => {
-  io.emit(result.lobby, result.drawing);
-};
-
 const socketSetup = (app) => {
   const server = http.createServer(app);
   io = new Server(server);
 
   io.on('connection', (socket) => {
     console.log('a user connected');
-
+    socket.join("room");
     socket.on('disconnect', () => {
       console.log('a user disconnected');
     });
 
-    socket.on('finished', handleResult);
+    socket.on('create room', (roomName) => {
+      socket.join(roomName);
+    });
+
+    socket.on('join room', (roomName) => {
+      socket.join(roomName);
+    });
+
+    socket.on('drawing', (drawing) => {
+      io.to("room").emit('drawing', drawing);
+    });
   });
 
   return server;

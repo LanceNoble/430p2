5157f3300395f730766792cfaee460f123b6e8a8
Lance <ltnlikespie@gmail.com>
2023-04-24 23:54:01 -0400

    Changed credentials error handling

--- client/pages/Credentials.jsx
+++ client/pages/Credentials.jsx
@@ -1,26 +1,21 @@
-const React = require('react');
+const React = require('react')
 
 export default function Credentials({ setPage }) {
-    const errorBox = React.useRef(null);
     return (
         <>
             <h1>Credentials Page</h1>
             <h2>Login</h2>
             <form onSubmit={async (e) => {
-                e.preventDefault();
-                const user = e.target.querySelector('input[type="text"]').value;
-                const pass = e.target.querySelector('input[type="password"]').value;
+                e.preventDefault()
+                const user = e.target.querySelector('input[type="text"]').value
+                const pass = e.target.querySelector('input[type="password"]').value
                 const res = await fetch('/session', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ user, pass }),
-                });
-                if (res.status === 204) {
-                    setPage("hub");
-                } else {
-                    errorBox.current.innerHTML = res.error;
-                }
-                return false;
+                })
+                res.status === 204 ? setPage("hub") : alert("Wrong username or password!")
+                return false
             }}>
                 <input type="text" placeholder="Username" required />
                 <input type="password" placeholder="Password" required />
@@ -28,34 +23,26 @@ export default function Credentials({ setPage }) {
             </form>
             <h2>Sign Up</h2>
             <form onSubmit={async (e) => {
-                e.preventDefault();
+                e.preventDefault()
                 const pass = e.target.querySelector("#pass").value
                 const passConfirm = e.target.querySelector("#passConfirm").value
                 if (pass === passConfirm) {
-                    const user = e.target.querySelector('input[type="text"]').value;
+                    const user = e.target.querySelector('input[type="text"]').value
                     const res = await fetch('/account', {
                         method: 'POST',
                         headers: { 'Content-Type': 'application/json' },
                         body: JSON.stringify({ user, pass }),
                     })
-                    if (res.status === 201) {
-                        setPage("hub");
-                    } else {
-                        errorBox.current.innerHTML = res.error;
-                    }
+                    res.status === 201 ? setPage("hub") : alert("Invalid username!")
                 }
-                else {
-                    errorBox.current.innerHTML = "Passwords do not match"
-                }
-                return false;
+                else alert("Passwords do not match!")
+                return false
             }}>
                 <input type="text" placeholder="Username" required />
                 <input id="pass" type="password" placeholder="Password" required />
                 <input id="passConfirm" type="password" placeholder="Retype Password" required />
                 <input type="submit" value="Sign Up" />
             </form>
-            <h2>Errors</h2>
-            <p ref={errorBox}></p>
         </>
-    );
+    )
 }

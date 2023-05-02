114aeeeb7ad7aa16c9f7e81058d2277c9e1cf33e
Lance <ltnlikespie@gmail.com>
2023-05-02 03:20:45 -0400

    Fixed hub not working after navigating back to it from leaderboard page

--- client/pages/Leaderboard.jsx
+++ client/pages/Leaderboard.jsx
@@ -1,37 +1,36 @@
 const React = require('react')
 export default function Leaderboard({ setPage }) {
-    React.useEffect(async () => {
+    React.useEffect(() => {
         const list = document.querySelector('ol')
-        const res = await fetch('/account', {
+        fetch('/account', {
             method: 'GET',
             headers: { 'Accept': 'application/json' }
-        })
-        const resJSON = await res.json()
-
-        // Sort users from highest to lowest wins
-        let highest = resJSON[0];
-        let indexHighest = 0;
-        let locked = 0;
-        while (locked < resJSON.length) {
-            for (let i = locked; i < resJSON.length; i++) {
-                if (resJSON[i].wins > highest.wins) {
-                    highest = resJSON[i];
-                    indexHighest = i;
+        }).then(res => res.json()).then(resJSON => {
+            // Sort users from highest to lowest wins
+            let highest = resJSON[0];
+            let indexHighest = 0;
+            let locked = 0;
+            while (locked < resJSON.length) {
+                for (let i = locked; i < resJSON.length; i++) {
+                    if (resJSON[i].wins > highest.wins) {
+                        highest = resJSON[i];
+                        indexHighest = i;
+                    }
                 }
-            }
 
-            let copy = resJSON[locked]
-            resJSON[locked] = highest
-            resJSON[indexHighest] = copy
-            locked++
-            highest = resJSON[locked]
-            indexHighest = locked
-        }
-        for (let i = 0; i < resJSON.length; i++) {
-            const li = document.createElement('li')
-            li.innerHTML = `${resJSON[i].user} - ${resJSON[i].wins} wins`
-            list.appendChild(li)
-        }
+                let copy = resJSON[locked]
+                resJSON[locked] = highest
+                resJSON[indexHighest] = copy
+                locked++
+                highest = resJSON[locked]
+                indexHighest = locked
+            }
+            for (let i = 0; i < resJSON.length; i++) {
+                const li = document.createElement('li')
+                li.innerHTML = `${resJSON[i].user} - ${resJSON[i].wins} wins`
+                list.appendChild(li)
+            }
+        })
     })
     return (
         <>
